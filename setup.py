a7fb32c322abdcb377175510d6c907a9e625c434
David Baldsiefen <david.baldsiefen@gmx.de>
2023-04-21 18:18:21 -0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=['ml_casadi'],
+    packages=['ml_casadi', 'ml_casadi.torch', 'ml_casadi.common', 'ml_casadi.torch.modules', 'ml_casadi.torch.modules.nn', 'ml_casadi.torch.math', 'ml_casadi.torch.matrix', 'ml_casadi.torch.autograd'],
     package_dir={'': '.'}
 )
 
